938019a827fd5ec1ef5772e424dba533f3089e3d
ctabin10 <ctabin10@gmail.com>
2026-04-15 21:02:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Doofenshmirtz-Evil-Inc.
+An intentionally insecure website for our Cyber Operations group project.
+https://dvizee.github.io/Doofenshmirtz-Evil-Inc./
---
+++ js/database.js
@@ -0,0 +1,15 @@
+
+
+
+export const users = {
+    "doof": "EVIL",
+    "perry": "theplatypus",
+    "admin": "admin",
+    "test" : "test"
+};
+
+
+export const creditcards = {
+    "doof": "4444 5555 6666 7777",
+    "vanessa": "1010 1010 1010 1010"
+}
